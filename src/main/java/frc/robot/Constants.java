719bef6aa2de65f79765b58dc61e699c097353f1
Jacob Barta <jacob.barta25@gmail.com>
2023-02-11 10:41:22 -0500

    write new dt code

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,16 @@
 
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
+import edu.wpi.first.math.geometry.*;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.math.util.Units;
+import frc.robot.ShamLib.motors.PIDFGains;
+
+import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -15,5 +25,129 @@ package frc.robot;
 public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
+  }
+
+
+  public static final SupplyCurrentLimitConfiguration CURRENT_LIMIT = new SupplyCurrentLimitConfiguration(true, 20, 20, 0.1);
+
+  public static final class ControllerConversions{
+    public static final double DEADBAND = 0.025;
+    public static final UnaryOperator<Double> conversionFunction = (input) -> (Math.copySign(input * input, input));
+  }
+
+  public static final class SwerveDrivetrain {
+
+    //TODO: Remove
+    public static final Pose3d limelightPose = new Pose3d(Units.inchesToMeters(-5), 0, Units.inchesToMeters(31), new Rotation3d());
+
+    // Distance between centers of right and left wheels on robot in meters
+    public static final double TRACK_WIDTH = Units.inchesToMeters(18.75);
+    // Distance between front and back wheels on robot in meters
+    public static final double WHEEL_BASE = Units.inchesToMeters(18.75);
+
+    // Maximum linear chassis speed in meters per second (MK4 standard modules capable of 4.1)
+    public static final double MAX_LINEAR_SPEED = 2;
+    public static final double MAX_LINEAR_ACCELERATION = 4;
+
+    // Maximum chassis rotational speed in radians per second
+    public static final double rotationRadius = Math.sqrt(Math.pow(TRACK_WIDTH / 2.0, 2) + Math.pow(WHEEL_BASE / 2.0, 2)) * 2 * Math.PI;
+    public static final double MAX_ROTATION = (MAX_LINEAR_SPEED / rotationRadius) * (2 * Math.PI);
+    public static final double MAX_ROT_ACCEL = MAX_ROTATION * 3;
+
+    //TODO: figure out which is which
+    public static final Translation2d[] moduleOffsets = {
+            new Translation2d(WHEEL_BASE / 2, TRACK_WIDTH / 2), //front left
+            new Translation2d(WHEEL_BASE / 2, -TRACK_WIDTH / 2), //back left
+            new Translation2d(-WHEEL_BASE / 2, -TRACK_WIDTH / 2), //
+            new Translation2d(-WHEEL_BASE / 2, TRACK_WIDTH / 2)
+    };
+
+    public static final SwerveDriveKinematics kDriveKinematics = new SwerveDriveKinematics(moduleOffsets);
+
+    public static final double P_HOLDANGLETELE = 3.5; //.5
+    public static final double I_HOLDANGLETELE = 0.25;
+    public static final double D_HOLDANGLETELE = 0;
+
+    public static final double P_HOLDANGLEAUTO = 5; //4
+    public static final double I_HOLDANGLEAUTO = .25; //.25
+    public static final double D_HOLDANGLEAUTO = 0;
+
+    public static final double P_HOLDTRANSLATION = 1; //1
+    public static final double I_HOLDTRANSLATION = 0;
+    public static final double D_HOLDTRANSLATION = 0;
+
+    public static final SwerveModuleState STOPPED_STATE = new SwerveModuleState(0, new Rotation2d());
+
+    public static final SwerveModuleState[] X_SHAPE_ARRAY = {
+            new SwerveModuleState(0, Rotation2d.fromDegrees(45)),
+            new SwerveModuleState(0, Rotation2d.fromDegrees(-45)),
+            new SwerveModuleState(0, Rotation2d.fromDegrees(45)),
+            new SwerveModuleState(0, Rotation2d.fromDegrees(-45))
+    };
+
+    public static final int PIGEON_ID = 01;
+
+    public static final int MODULE_1_DRIVE_ID = 11;
+    public static final int MODULE_1_TURN_ID = 12;
+    public static final int MODULE_1_ENCODER_ID = 11;
+    public static final double MODULE_1_OFFSET = 76.7;
+
+    public static final int MODULE_2_DRIVE_ID = 13;
+    public static final int MODULE_2_TURN_ID = 14;
+    public static final int MODULE_2_ENCODER_ID = 13;
+    public static final double MODULE_2_OFFSET = 156.6;
+
+    public static final int MODULE_3_DRIVE_ID = 15;
+    public static final int MODULE_3_TURN_ID = 16;
+    public static final int MODULE_3_ENCODER_ID = 15;
+    public static final double MODULE_3_OFFSET = 59.8;
+
+    public static final int MODULE_4_DRIVE_ID = 17;
+    public static final int MODULE_4_TURN_ID = 18;
+    public static final int MODULE_4_ENCODER_ID = 17;
+    public static final double MODULE_4_OFFSET = -153.1;
+
+    public static Supplier<Pose2d> getOdoPose;
+    public static Supplier<Rotation2d> getDrivetrainAngle;
+  }
+
+  public static final class SwerveModule {
+
+    // Drive motor PID controller coefficients
+    // public static final double P_DRIVE = 0.175;
+    public static final double P_DRIVE = 0.0;
+    public static final double I_DRIVE = 0;
+    public static final double D_DRIVE = 0;
+    public static final double KF_DRIVE = 0.0679;
+
+    // Use this ratio to convert from Falcon angular velocity to wheel angular velocity
+    public static final double DRIVE_RATIO = 8.14;
+
+    // Turn motor PID controller coefficients
+    // public static final double P_TURN = 9;
+    public static final double P_TURN = 0.6;
+    public static final double I_TURN = 0;
+    public static final double D_TURN = 0; //0.1
+    public static final double KF_TURN = 0.06464446306847016; //0.15
+    public static final double MAX_TURN_SPEED = 50; // Rad/S //50
+    public static final double MAX_TURN_ACCEL = 400; // Rad/S^2
+
+    public static final PIDFGains TURN_GAINS = new PIDFGains(
+            P_TURN,
+            I_TURN,
+            D_TURN,
+            KF_TURN
+    );
+
+    public static final PIDFGains DRIVE_GAINS = new PIDFGains(
+            P_DRIVE,
+            I_DRIVE,
+            D_DRIVE,
+            KF_DRIVE
+    );
+
+    public static final int kTimeoutMs = 30;//TODO: change these if needed vv
+    public static final int kPIDLoopIdx = 0;
+    public static final int kSlotIdx = 0;
   }
 }
---
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,180 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.util.sendable.SendableBuilder;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.Constants;
+import frc.robot.ShamLib.PIDGains;
+import frc.robot.ShamLib.SMF.StateMachine;
+import frc.robot.ShamLib.swerve.DriveCommand;
+import frc.robot.ShamLib.swerve.ModuleInfo;
+import frc.robot.ShamLib.swerve.SwerveDrive;
+
+import static frc.robot.Constants.SwerveDrivetrain.*;
+import static frc.robot.Constants.SwerveModule.*;
+
+public class Drivetrain extends StateMachine<Drivetrain.DrivetrainState> {
+    private final SwerveDrive drive;
+    private final CommandXboxController controller;
+
+//    private final Limelight ll = new Limelight();
+
+    private final Transform3d llToBot = new Transform3d(new Pose3d(), Constants.SwerveDrivetrain.limelightPose).inverse();
+
+    public Drivetrain(CommandXboxController controller) {
+        super("Drivetrain", DrivetrainState.Undetermined, DrivetrainState.class);
+
+        drive = new SwerveDrive(
+                PIGEON_ID,
+                DRIVE_GAINS,
+                TURN_GAINS,
+                MAX_LINEAR_SPEED,
+                MAX_LINEAR_ACCELERATION,
+                MAX_TURN_SPEED,
+                new PIDGains(P_HOLDANGLETELE, I_HOLDANGLETELE, D_HOLDANGLETELE),
+                new PIDGains(P_HOLDANGLEAUTO, I_HOLDANGLEAUTO, D_HOLDANGLEAUTO),
+                new PIDGains(P_HOLDTRANSLATION, I_HOLDTRANSLATION, D_HOLDTRANSLATION),
+                true,
+                "Drivetrain",
+                "",
+                Constants.CURRENT_LIMIT,
+                ModuleInfo.getMK4IL1Module(MODULE_1_DRIVE_ID, MODULE_1_TURN_ID, MODULE_1_ENCODER_ID, MODULE_1_OFFSET, moduleOffsets[0], false),
+                ModuleInfo.getMK4IL1Module(MODULE_2_DRIVE_ID, MODULE_2_TURN_ID, MODULE_2_ENCODER_ID, MODULE_2_OFFSET, moduleOffsets[1], false),
+                ModuleInfo.getMK4IL1Module(MODULE_3_DRIVE_ID, MODULE_3_TURN_ID, MODULE_3_ENCODER_ID, MODULE_3_OFFSET, moduleOffsets[2], false),
+                ModuleInfo.getMK4IL1Module(MODULE_4_DRIVE_ID, MODULE_4_TURN_ID, MODULE_4_ENCODER_ID, MODULE_4_OFFSET, moduleOffsets[3], false)
+        );
+
+        this.controller = controller;
+
+        defineTransitions();
+        defineStateCommands();
+
+        SmartDashboard.putData(drive.getField());
+    }
+
+    private void defineTransitions() {
+        addOmniTransition(
+                DrivetrainState.XShape,
+                new InstantCommand(() -> setModuleStates(X_SHAPE_ARRAY))
+        );
+
+        addOmniTransition(
+                DrivetrainState.Idle,
+                new InstantCommand(() -> {
+                    setAllModules(STOPPED_STATE);
+                    stopModules();
+                })
+        );
+
+        addTransition(
+                DrivetrainState.Idle,
+                DrivetrainState.TeleopDrive,
+                new InstantCommand()
+        );
+
+        addTransition(
+                DrivetrainState.XShape,
+                DrivetrainState.TeleopDrive,
+                new InstantCommand()
+        );
+    }
+
+    private void defineStateCommands() {
+        registerStateCommand(
+                DrivetrainState.TeleopDrive,
+                new DriveCommand(
+                        drive,
+                        () -> -controller.getLeftY(),
+                        () -> -controller.getLeftX(),
+                        () -> -controller.getRawAxis(2),
+                        MAX_LINEAR_SPEED,
+                        MAX_LINEAR_ACCELERATION,
+                        MAX_ROTATION,
+                        MAX_ROT_ACCEL,
+                        Constants.ControllerConversions.DEADBAND,
+                        Constants.ControllerConversions.conversionFunction,
+                        true,
+                        this
+                )
+        );
+    }
+
+    public boolean isFieldRelative() {
+        return drive.isFieldRelative();
+    }
+
+    public void setFieldRelative(boolean v) {
+        drive.setFieldRelative(v);
+    }
+
+    public void updateOdometry() {
+        drive.updateOdometry();
+        drive.updateField2dObject();
+
+//        if(ll.hasTarget()) {
+//            Pose3d rawLLPose = ll.getPose3d();
+//
+//
+//            drive.addVisionMeasurement(rawLLPose.transformBy(llToBot).toPose2d());
+//        }
+    }
+
+    public void drive(ChassisSpeeds speeds, boolean allowHoldAngleChange) {
+        drive.drive(speeds, allowHoldAngleChange);
+    }
+
+    public void setModuleStates(SwerveModuleState[] states) {
+        drive.setModuleStates(states);
+    }
+
+    public void setAllModules(SwerveModuleState state) {
+        drive.setAllModules(state);
+    }
+
+    public Rotation2d getCurrentAngle() {
+        return drive.getCurrentAngle();
+    }
+
+    public void stopModules() {
+        drive.stopModules();
+    }
+
+    public void resetGyro(Rotation2d angle) {
+        drive.resetGyro(angle);
+    }
+
+    @Override
+    protected void onEnable() {
+
+    }
+
+    @Override
+    protected void onDisable() {
+        requestTransition(DrivetrainState.Idle);
+    }
+
+    @Override
+    protected void update() {
+        updateOdometry();
+    }
+
+    @Override
+    protected void determineSelf() {
+        setState(DrivetrainState.Idle);
+    }
+
+    @Override
+    protected void additionalSendableData(SendableBuilder builder) {
+        builder.addDoubleArrayProperty("absolute angles", drive::getModuleAbsoluteAngles, null);
+    }
+
+    public enum DrivetrainState {
+        Undetermined, XShape, TeleopDrive, Idle
+    }
+}
