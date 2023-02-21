3fe50429f518107c1c2fae9e7b441aabda8b5e65
Jacob Barta <jacob.barta25@gmail.com>
2023-02-21 10:24:21 -0500

    add comment to express my disapproval

--- src/main/java/frc/robot/subsystems/Claw.java
+++ src/main/java/frc/robot/subsystems/Claw.java
@@ -22,6 +22,7 @@ public class Claw extends StateMachine<Claw.State> {
         addOmniTransition(State.OPENED, new InstantCommand(() -> solenoid.set(SOLENOID_CLAW_OPEN_STATE)));
     }
 
+    //TODO: This is probably not the correct behavior, but we can leave it for now
     @Override
     protected void onTeleopStart() {
         requestTransition(State.OPENED);
