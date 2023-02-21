93ff295383e4d994f8f2141f94bf947a21f8fd81
Jacob Barta <jacob.barta25@gmail.com>
2023-02-21 13:48:14 -0500

    finish base vision class. Should be fully featured now

--- src/main/java/frc/robot/subsystems/BaseVision.java
+++ src/main/java/frc/robot/subsystems/BaseVision.java
@@ -9,28 +9,37 @@ import java.util.function.Supplier;
 public class BaseVision extends SubsystemBase {
 
     private final Limelight ll = new Limelight("base-ll");
+    private final Pose3d initialCameraPose;
+    private final Supplier<Rotation2d> turretAngleSupplier;
 
-    private Pose3d initialCameraPose;
-    private Supplier<Rotation2d> turretAngleSupplier;
-
+    /**
+     * Create a new instance of the limelight on the base of the robot
+     * @param initialCameraPose the pose of the camera with the turret at zero degrees
+     * @param turretAngleSupplier supplier for the angle of the turret
+     */
     public BaseVision(Pose3d initialCameraPose, Supplier<Rotation2d> turretAngleSupplier) {
+        this.initialCameraPose = initialCameraPose;
 
-
+        this.turretAngleSupplier = turretAngleSupplier;
     }
 
-    public Pose3d getPose3D(Pose3d limelightPose) {
+
+    /**
+     * Get the current pose of the limelight reading
+     * @return pose of the limelight
+     */
+    public Pose3d getPose3D() {
         Translation3d initialTranslation = initialCameraPose.getTranslation();
 
-        Translation3d newTranslation = initialTranslation.rotateBy(new Rotation3d(0, 0, turretAngleSupplier.get().getRadians()))
+        //Calculate the translation for where the camera actually is relative to the bot
+        Translation3d newTranslation = initialTranslation.rotateBy(new Rotation3d(0, 0, turretAngleSupplier.get().getRadians()));
 
+        //This pose has the correct translation for the camera
+        Pose3d poseWithCorrectTranslation = ll.getPose3d().transformBy(new Transform3d(newTranslation, new Rotation3d()));
 
-        Transform3d transformBy = new Transform3d(
-                new Pose3d(
-                        new Translation3d(),
-                        new Rotation3d(0, 0, turretAngleSupplier.get().getRadians())),
-                initialCameraPose).inverse();
+        //Rotate the camera to face the correct way
 
-        return ll.getPose3d().transformBy(transformBy);
+        return endPoint;
     }
 
 
