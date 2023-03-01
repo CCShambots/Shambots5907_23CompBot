3c2b761dae1500cc7f6a048b42935fbc99a2d5eb
Jacob Barta <jacob.barta25@gmail.com>
2023-03-01 09:13:13 -0500

    remove some whitespace

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,9 +6,11 @@ package frc.robot;
 
 import com.pathplanner.lib.server.PathPlannerServer;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.ShamLib.SMF.SubsystemManagerFactory;
+import frc.robot.ShamLib.sensor.ThroughBoreEncoder;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -19,7 +21,7 @@ import frc.robot.ShamLib.SMF.SubsystemManagerFactory;
 public class Robot extends TimedRobot {
   private Command autonomousCommand;
   private RobotContainer robotContainer;
-
+  
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
@@ -48,7 +50,7 @@ public class Robot extends TimedRobot {
     // and running subsystem periodic() methods.  This must be called from the robot's periodic
     // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
-  }
+   }
 
   /** This function is called once each time the robot enters Disabled mode. */
   @Override
