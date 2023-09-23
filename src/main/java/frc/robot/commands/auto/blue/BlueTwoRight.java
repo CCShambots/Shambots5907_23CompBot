533a05e3c6426e3e7fdd6e48ad3b894c2a03e3df
Jacob Barta <jacob.barta25@gmail.com>
2023-09-23 12:06:52 -0400

    make bump side autos that actually work

--- src/main/java/frc/robot/commands/auto/blue/BlueTwoRight.java
+++ src/main/java/frc/robot/commands/auto/blue/BlueTwoRight.java
@@ -22,31 +22,39 @@ public class BlueTwoRight extends BaseAutoRoute {
                 rc.waitForReady(),
                 new ScoreFirstElementCommand(rc),
 
-                rc.cv().transitionCommand(VisionState.CONE_DETECTOR),
-                new ParallelCommandGroup(
-                        rc.runTraj("blue-get-element-right", true),
-                        new SequentialCommandGroup(
-                                new WaitCommand(1),
-                                rc.arm().setArmNormalSpeedCommand(),
-                                rc.arm().transitionCommand(ArmMode.SEEKING_PRIMED),
-                                rc.arm().waitForState(ArmMode.PRIMED),
-                                new WaitCommand(0.5),
-                                rc.turret().goToAngle(Math.toRadians(-90)),
-                                new WaitCommand(0.5),
-                                rc.arm().setArmSlowSpeedCommand(),
-                                rc.arm().openClaw(),
-                                rc.arm().transitionCommand(Arm.ArmMode.NEW_GROUND_INTERMEDIATE),
-                                new WaitCommand(1.5),
-                                rc.arm().transitionCommand(ArmMode.NEW_GROUND_PICKUP)    
-                        )
-                ),
-                rc.dt().waitForState(DrivetrainState.IDLE),
-                new GrabSequenceCommand(rc),
-                rc.arm().transitionCommand(ArmMode.LOW_SCORE),
-                rc.turret().goToAngle(Math.toRadians(90)),
-                rc.runTraj("blue-score-element-right"),
-                new WaitCommand(2.5),
-                rc.arm().openClaw()
+                rc.runTraj("blue-get-element-right", true),
+                new WaitCommand(1),
+                rc.arm().setArmNormalSpeedCommand(),
+                rc.arm().transitionCommand(ArmMode.SEEKING_STOWED),
+                rc.arm().waitForState(ArmMode.STOWED),
+                new WaitCommand(0.5),
+                rc.turret().goToAngle(Math.toRadians(-90))
+
+//                rc.cv().transitionCommand(VisionState.CONE_DETECTOR),
+//                new ParallelCommandGroup(
+//                        rc.runTraj("blue-get-element-right", true),
+//                        new SequentialCommandGroup(
+//                                new WaitCommand(1),
+//                                rc.arm().setArmNormalSpeedCommand(),
+//                                rc.arm().transitionCommand(ArmMode.SEEKING_PRIMED),
+//                                rc.arm().waitForState(ArmMode.PRIMED),
+//                                new WaitCommand(0.5),
+//                                rc.turret().goToAngle(Math.toRadians(-90)),
+//                                new WaitCommand(0.5),
+//                                rc.arm().setArmSlowSpeedCommand(),
+//                                rc.arm().openClaw(),
+//                                rc.arm().transitionCommand(Arm.ArmMode.NEW_GROUND_INTERMEDIATE),
+//                                new WaitCommand(1.5),
+//                                rc.arm().transitionCommand(ArmMode.NEW_GROUND_PICKUP)
+//                        )
+//                ),
+//                rc.dt().waitForState(DrivetrainState.IDLE),
+//                new GrabSequenceCommand(rc),
+//                rc.arm().transitionCommand(ArmMode.LOW_SCORE),
+//                rc.turret().goToAngle(Math.toRadians(90)),
+//                rc.runTraj("blue-score-element-right"),
+//                new WaitCommand(2.5),
+//                rc.arm().openClaw()
         );
     }
 }
